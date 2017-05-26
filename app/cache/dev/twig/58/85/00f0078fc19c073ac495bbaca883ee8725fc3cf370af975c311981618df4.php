f7980a65447e480b13f5bf08ace88ad948513b8a
elchicoderoque <pellotl1328@gmail.com>
2017-05-26 10:20:20 -0500

    hola

---
+++ app/cache/dev/twig/58/85/00f0078fc19c073ac495bbaca883ee8725fc3cf370af975c311981618df4.php
@@ -0,0 +1,74 @@
+<?php
+
+/* CursoMainBundle:Default:articulos.html.twig */
+class __TwigTemplate_588500f0078fc19c073ac495bbaca883ee8725fc3cf370af975c311981618df4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "
+<h1>Listado de Articulos</h1>
+<h2>Listado 222222223333</h2>
+<table border=\"1\">
+    <tr>
+        <th>ID</th>
+        <th>Titulo</th>
+        <th>Fecha de Creacion</th>
+          <th>Fati</th>
+          <th>Chava</th>
+    </tr>
+    ";
+        // line 12
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["articulos"]) ? $context["articulos"] : $this->getContext($context, "articulos")));
+        foreach ($context['_seq'] as $context["_key"] => $context["articulo"]) {
+            // line 13
+            echo "    <tr>
+        <td>";
+            // line 14
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["articulo"]) ? $context["articulo"] : $this->getContext($context, "articulo")), "id"), "html", null, true);
+            echo "</td>
+        <td>";
+            // line 15
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["articulo"]) ? $context["articulo"] : $this->getContext($context, "articulo")), "title"), "html", null, true);
+            echo "</td>
+        <td>";
+            // line 16
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["articulo"]) ? $context["articulo"] : $this->getContext($context, "articulo")), "created"), "html", null, true);
+            echo "</td>
+    </tr>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['articulo'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 19
+        echo "</table>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CursoMainBundle:Default:articulos.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  56 => 19,  47 => 16,  43 => 15,  39 => 14,  36 => 13,  32 => 12,  19 => 1,);
+    }
+}
